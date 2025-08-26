6b928e19e7e871b21e9bf44f116ac9cde8fae452
ParasJain12 <parasjain8103@gmail.com>
2025-08-27 00:53:47 +0530

    Abstract Factory Pattern

---
+++ src/main/java/com/pattern/designpattern/AbstractFactoryPatternDemo.java
@@ -0,0 +1,79 @@
+package com.pattern.designpattern;
+
+interface Button{
+    void click();
+}
+
+interface Checkbox{
+    void check();
+}
+
+class WinButton implements Button{
+    public void click(){
+        System.out.println("Windows Button clicked");
+    }
+}
+
+class WinCheckbox implements Checkbox{
+    public void check(){
+        System.out.println("Windows Checkbox checked");
+    }
+}
+
+class MacButton implements Button{
+    public void click(){
+        System.out.println("Mac Button clicked");
+    }
+}
+
+class MacCheckbox implements Checkbox{
+    public void check(){
+        System.out.println("Mac Checkbox checked");
+    }
+}
+
+// Abstract factory
+interface GUIFactory{
+    Button createButton();
+    Checkbox createCheckbox();
+}
+
+class WinFactory implements GUIFactory{
+    public Button createButton(){
+        return new WinButton();
+    }
+    
+    public Checkbox createCheckbox(){
+        return new WinCheckbox();
+    }
+}
+
+class MacFactory implements GUIFactory{
+    public Button createButton(){
+        return new MacButton();
+    }
+    
+    public Checkbox createCheckbox(){
+        return new MacCheckbox();
+    }
+}
+
+// Client code use factory, not concreate classes
+public class AbstractFactoryPatternDemo {
+    
+    public static void main(String[] args) {
+        GUIFactory factory = new WinFactory();
+        Button btn = factory.createButton();
+        Checkbox chk = factory.createCheckbox();
+        
+        btn.click();
+        chk.check();
+        
+        factory = new MacFactory();
+        Button btn1 = factory.createButton();
+        Checkbox chk1 = factory.createCheckbox();
+        
+        btn1.click();
+        chk1.check();
+    }
+}
